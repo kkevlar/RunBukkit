6c92a14278c4c486ef182db4d4c67a2ef47d7979
Kevin Kellar <kellar.kevin@gmail.com>
2016-01-17 11:26:25 -0800

    Worked with reader

---
+++ src/com/flipturnapps/runcbukkit/CBukkitListener.java
@@ -0,0 +1,5 @@
+package com.flipturnapps.runcbukkit;
+
+public interface CBukkitListener {
+
+}
--- src/com/flipturnapps/runcbukkit/Reader.java
+++ src/com/flipturnapps/runcbukkit/Reader.java
@@ -1,5 +1,46 @@
 package com.flipturnapps.runcbukkit;
 
-public class Reader {
+import java.io.BufferedReader;
+import java.util.ArrayList;
 
+public class Reader implements Runnable
+{
+	private BufferedReader stream;
+	private ArrayList<CBukkitListener> listeners;
+	public Reader(BufferedReader reader)
+	{
+		stream = reader;
+		setListeners(new ArrayList<CBukkitListener>());
+	}
+	public void start()
+	{
+		new Thread(this).start();
+	}
+	
+	@Override
+	public void run() 
+	{
+		while(true)
+		{
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String line = stream.readLine();
+			if(line != null)
+			{
+				for
+			}
+		}
+		
+	}
+	public ArrayList<CBukkitListener> getListeners() {
+		return listeners;
+	}
+	public void setListeners(ArrayList<CBukkitListener> listeners) {
+		this.listeners = listeners;
+	}
+	
 }
