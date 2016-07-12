238b62dada62496d2fdd7f3e94b47d273eccc19d
Kevin Kellar <kellar.kevin@gmail.com>
2016-07-12 10:25:23 -0700

    Added some meat to client

--- src/com/flipturnapps/runcbukkit/Client.java
+++ src/com/flipturnapps/runcbukkit/Client.java
@@ -6,11 +6,43 @@ import java.net.Socket;
 import com.flipturnapps.kevinLibrary.net.ClientData;
 import com.flipturnapps.kevinLibrary.net.KServer;
 
-public class Client extends ClientData {
-
-	public Client(Socket socket, KServer<?> server) throws IOException {
+public class Client extends ClientData
+{
+	private long verboseTime;
+	public Client(Socket socket, KServer<?> server) throws IOException 
+	{
 		super(socket, server);
-		// TODO Auto-generated constructor stub
 	}
 
+	public void sendMessageToServer(String message)
+	{
+		if (message.toLowerCase().startsWith("verbose"))
+		{
+			String[] split = message.replace(" ", "=").replace("=","~").split("~");
+			long add = Long.parseLong(split[1]);
+			addVerboseTime(add);
+		}
+		else
+		super.sendMessageToServer(message);
+	}
+
+	private void addVerboseTime(long add)
+	{
+		if(this.getVerboseTime() < System.currentTimeMillis())
+			this.setVerboseTime(System.currentTimeMillis());
+		this.setVerboseTime(this.getVerboseTime() + add);
+	}
+
+	public long getVerboseTime() {
+		return verboseTime;
+	}
+
+	public void setVerboseTime(long verboseTime) {
+		this.verboseTime = verboseTime;
+	}
+	public void sendDumpIfVerbose(String line)
+	{
+		if(this.getVerboseTime() >= System.currentTimeMillis())
+			this.sendMessage(line);
+	}
 }
